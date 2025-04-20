44e9da0bc4ab8d9e69547225c26473676044de3a
Xavier <tim88776@gmail.com>
2025-04-20 11:04:33 +0800

    feat: add Contact model

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from config import Config
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+with app.app_context():
+      db.create_all()
 
 if __name__ == "__main__":
       app.run(debug=True)
